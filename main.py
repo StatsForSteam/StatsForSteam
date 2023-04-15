0911ffdb740f8c0802e603d04fae428a094b0bfb
Callum <callumbrezden@gmail.com>
2023-04-14 19:42:01 -0700

    Integrating OpenID Login with Steam v2

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
-from flask import Flask, render_template, request, g, session, flash, \
-     redirect, url_for, abort
-from flask_openid import OpenID
-from openid.extensions import pape
+from flask import Flask, redirect, request, render_template, session, url_for
+from json import dumps, loads
+from urllib.parse import urlencode
 
 app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
-app.config.update(
-    SECRET_KEY = 'development key',
-    DEBUG = True
-)
+app.debug = True
+app.config['SESSION_TYPE'] = 'filesystem'
+app.secret_key = 'random'
+steam_openid_url = 'https://steamcommunity.com/openid/login'
 
-oid = OpenID(app, safe_roots=[], extension_responses=[pape.Response])
+@app.before_first_request
+def before_first_request():
+    session['user'] = 'Log into steam!'
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route("/")
+def root():
+    return render_template('index.html', user=session['user'])
 
-@app.post('/')
-@oid.loginhandler
-def login():
-    return oid.try_login("https://steamcommunity.com/openid", extensions=[pape.Request([])])
-    
-@oid.after_login
-def create_or_login(resp):
-    # We should be able to get the OpenID in here.
-    key = resp.identity_url
-    pass
+@app.route("/auth")
+def auth_with_steam():
 
-if __name__ == '__main__':
+    params = {
+        'openid.ns': "http://specs.openid.net/auth/2.0",
+        'openid.identity': "http://specs.openid.net/auth/2.0/identifier_select",
+        'openid.claimed_id': "http://specs.openid.net/auth/2.0/identifier_select",
+        'openid.mode': 'checkid_setup',
+        'openid.return_to': 'http://127.0.0.1:5000/authorize',
+        'openid.realm': 'http://127.0.0.1:5000'
+    }
+
+    query_string = urlencode(params)
+    auth_url = steam_openid_url + "?" + query_string
+    return redirect(auth_url)
+
+@app.route("/authorize")
+def authorize():
+    jsonData = dumps(request.args)
+    data = loads(jsonData)
+    session['user'] = data['openid.claimed_id']
+    return redirect(url_for('root'))
+
+if __name__ == "__main__":
     app.run()
