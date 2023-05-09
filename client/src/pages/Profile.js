b88fc48ee05d454b432954f698430a0acfe4d6af
AndrewWMeier <123119277+AndrewWMeier@users.noreply.github.com>
2023-05-09 16:10:14 -0700

    i forgor

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -62,10 +62,3 @@ function Profile() {
 
 export default Profile;
 
-// {/* <Form.Group className="mb-3" controlId="formBasicEmail">
-//         <Form.Label>Email address</Form.Label>
-//         <Form.Control type="input" placeholder="Enter email" />
-//         <Form.Text className="text-muted">
-//           We'll never share your email with anyone else.
-//         </Form.Text>
-//       </Form.Group> */}
