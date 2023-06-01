ad2ddf5a267003d2ec0730b0d42a9be7d8f7b165
Callum <callumbrezden@gmail.com>
2023-06-01 16:40:12 -0700

    Changed all routes to /api and added library

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,22 @@
 import OpenIDAuth, json, getData
+import flask_praetorian
 from database import mysql
 from flask import Flask
 from flask_session import Session
 from flask_cors import CORS
 
 app = Flask(__name__)
+app.debug = True
 
 Session(app)
 CORS(app)
+
+# JWT
+guard = flask_praetorian.Praetorian()
+guard.init_app(app)
+app.config["SECRET_KEY"] = "top secret"
+app.config["JWT_ACCESS_LIFESPAN"] = {"hours": 24}
+app.config["JWT_REFRESH_LIFESPAN"] = {"days": 30}
 
 # Database Connection
 with open('DatabaseCredentials.json') as DatabaseCredentials:
@@ -21,16 +30,16 @@ app.config['MYSQL_DB'] = DatabaseCredentialsJson['DB']
 mysql.init_app(app)
 
 # API Routes
-app.add_url_rule('/checkUserStatus', view_func=OpenIDAuth.checkUserStatus)
-app.add_url_rule('/userAuthentication', view_func=OpenIDAuth.userAuthentication)
-app.add_url_rule('/login', view_func=OpenIDAuth.login)
-app.add_url_rule('/logout', view_func=OpenIDAuth.logout)
-app.add_url_rule('/authorize', view_func=OpenIDAuth.authorize)
-app.add_url_rule('/getUserName', view_func=getData.getUserName)
-app.add_url_rule('/getSessionSID', view_func=getData.getSessionSID)
-app.add_url_rule('/getUserProfilePicture', view_func=getData.getUserProfilePicture)
-app.add_url_rule('/getUserGames', view_func=getData.getUserGames)
-app.add_url_rule('/getAchievements', view_func=getData.getAchievements, methods=["POST"])
+app.add_url_rule('/api/checkUserStatus', view_func=OpenIDAuth.checkUserStatus)
+app.add_url_rule('/api/userAuthentication', view_func=OpenIDAuth.userAuthentication)
+app.add_url_rule('/api/login', view_func=OpenIDAuth.login)
+app.add_url_rule('/api/logout', view_func=OpenIDAuth.logout)
+app.add_url_rule('/api/authorize', view_func=OpenIDAuth.authorize)
+app.add_url_rule('/api/getUserName', view_func=getData.getUserName)
+app.add_url_rule('/api/getSessionSID', view_func=getData.getSessionSID)
+app.add_url_rule('/api/getUserProfilePicture', view_func=getData.getUserProfilePicture)
+app.add_url_rule('/api/getUserGames', view_func=getData.getUserGames)
+app.add_url_rule('/api/getAchievements', view_func=getData.getAchievements, methods=["POST"])
 
 if __name__ == "__main__":
-    app.run(debug = True, use_reloader=True)
+    app.run(use_reloader=True)
